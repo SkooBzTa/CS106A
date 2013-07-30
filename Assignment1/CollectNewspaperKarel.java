6d0b515a1bf86f3fd33ca422e0e8e6d00fcb3645
Will Stevenson <skoobzta@gmail.com>
2013-07-30 20:01:10 +1000

    First 2 problems done

--- Assignment1/CollectNewspaperKarel.java
+++ Assignment1/CollectNewspaperKarel.java
@@ -13,5 +13,44 @@ import stanford.karel.*;
 public class CollectNewspaperKarel extends Karel {
 	
 	// You fill in this part
-
+	/*
+	 * Main program
+	 */
+	public void	run() {
+		moveToPaper();
+		pickBeeper();
+		returnToStart();
+	}
+	
+	private void moveToPaper() {
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+		move();
+	}
+	
+	private void returnToStart() {
+		turnAround();
+		move();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+		turnAround();
+	}
+	
+	private void turnRight() {
+		turnLeft();
+		turnLeft();
+		turnLeft();
+	}
+	
+	private void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
 }
+
--- Assignment1/StoneMasonKarel.java
+++ Assignment1/StoneMasonKarel.java
@@ -13,5 +13,72 @@ import stanford.karel.*;
 public class StoneMasonKarel extends SuperKarel {
 
 	// You fill in this part
+	public void run(){
+		fixColumn();
+		while(frontIsClear()){
+			nextColumn();
+		}
+		
+				
+	}
+	
+	private void fixColumn() {
+		/*
+		 * Repairs a column by ascending and placing beepers(bricks) then descending 
+		 */
+		ascendColumn();
+		descendColumn();
+	}
+	
+	private void ascendColumn() {
+		/*
+		 * Ascends the column looking for holes along the way,
+		 * fills any holes with a beeper (brick)
+		 */
+		while (notFacingNorth()) {
+			turnLeft();
+		}
+		while (frontIsClear()) {
+			checkForHole();
+			move();
+			}
+		checkForHole();
+	}
+	
+	private void descendColumn() {
+		/*
+		 * Descends the column all the way to the bottom
+		 */
+		while (notFacingSouth()) {
+			turnLeft();
+		}
+		while (frontIsClear()) {
+		move();
+		}
+		while (notFacingEast()) {
+		turnLeft();
+		}
+	}
+	
+	private void nextColumn() {
+		/*
+		 * Moves Karel to where the next column should be (providing he is standing at
+		 * the base of a column)
+		 */
+		for (int i = 0; i < 4; i++){
+			if (frontIsClear()) {
+				move();
+			}
+		}
+		fixColumn();
+	}
+
+	private void checkForHole() {
+		/*
+		 * Checks where Karel is standing, and if no brick is present, Karel will put one there
+		 */
+		if(noBeepersPresent())
+			putBeeper();
+	}
 
 }
